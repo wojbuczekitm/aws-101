d31af981a6bec4aa9885b3e79f07bd8a31987462
wojbuczekitm <wojciech.buczek@itmagination.com>
2022-04-01 17:33:21 +0200

    init

---
+++ containers/demo/app/WeatherApp.Api/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherApp.Api.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet("{name?}")]
+    public string Get([FromRoute] string? name=null)
+    {
+        return $"Hello {name ?? "World"}";
+    }
+}
